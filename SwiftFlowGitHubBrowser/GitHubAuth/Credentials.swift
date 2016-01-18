03cd67ae8a1e7785e7f22b263146bcdbc66715a2
Ben-G <Benjamin.Encz@gmail.com>
2016-01-18 11:48:32 -0800

    [GitHub] Add Empty Credentials File

---
+++ SwiftFlowGitHubBrowser/GitHubAuth/Credentials.swift
@@ -0,0 +1,12 @@
+//
+//  Credentials.swift
+//  SwiftFlowGitHubBrowser
+//
+//  Created by Benjamin Encz on 1/18/16.
+//  Copyright © 2016 Benji Encz. All rights reserved.
+//
+
+import Foundation
+
+let gitHubToken = // provide your token as string
+let gitHubSecret = // provide your secret as string
