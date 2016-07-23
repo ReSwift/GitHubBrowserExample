905fc19ef063115811351c4fd7646c9252da26c5
Benji Encz <Benjamin.Encz@gmail.com>
2016-07-23 15:00:57 -0700

    [RepositoryDetailView] Load Repository Details in WebView

--- SwiftFlowGitHubBrowser/ViewControllers/RepositoryDetailViewController.swift
+++ SwiftFlowGitHubBrowser/ViewControllers/RepositoryDetailViewController.swift
@@ -13,7 +13,7 @@ import ReSwiftRouter
 
 class RepositoryDetailViewController: UIViewController, StoreSubscriber {
 
-    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var webView: UIWebView!
     @IBOutlet var bookmarkButton: UIBarButtonItem!
 
     var repository: Repository?
@@ -46,9 +46,16 @@ class RepositoryDetailViewController: UIViewController, StoreSubscriber {
     }
 
     func newState(state: (selectedRepository: Repository?, bookmarks: [Bookmark])) {
-        self.repository = state.selectedRepository
-        self.mainLabel.text = state.selectedRepository?.name ?? ""
-        self.title = state.selectedRepository?.name ?? ""
+        // Only perform repository related updates if the repository actually changed
+        if self.repository?.gitURL != state.selectedRepository?.gitURL {
+            self.repository = state.selectedRepository
+            self.title = state.selectedRepository?.name ?? ""
+
+            if let url = state.selectedRepository?.htmlURL.flatMap({NSURL.init(string: $0)}) {
+                let request = NSURLRequest(URL: url)
+                self.webView.loadRequest(request)
+            }
+        }
 
         let bookmarkActive = !state.bookmarks.contains { route, data in
             guard let repository = data as? Repository else { return false }
